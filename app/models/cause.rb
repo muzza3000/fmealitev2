ca3ae2cbc202ade1e20eb17140b844f803e7919e
Thomas Murray <thomas.murray09@gmail.com>
2020-10-05 16:10:05 +0200

    basic model structure crash tested

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,5 +1,4 @@
 class Cause < ApplicationRecord
   belongs_to :failure_mode
-  belongs_to :fmea, through: :failure_mode
   has_many :actions
 end
--- app/models/effect.rb
+++ app/models/effect.rb
@@ -1,4 +1,3 @@
 class Effect < ApplicationRecord
   belongs_to :failure_mode
-  belongs_to :fmea, through: :failure_mode
 end
