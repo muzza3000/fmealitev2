191362385d4d4a9d5131d9fcdfe2a8ae440e02ab
Benjamin Bruckner <benjamin.brueckner@bluewin.ch>
2020-10-06 11:31:48 +0200

    fmea model cleaned for the activities dependencies and the too many destroy dependencies. tested, all works.

--- app/models/fmea.rb
+++ app/models/fmea.rb
@@ -1,11 +1,11 @@
 class Fmea < ApplicationRecord
   belongs_to :risk_matrix
-  has_many :activities, dependent: :destroy
+  has_many :activities
   has_many :functions, dependent: :destroy
-  has_many :failure_modes, through: :functions, dependent: :destroy
-  has_many :effects, through: :failure_modes, dependent: :destroy
-  has_many :causes, through: :failure_modes, dependent: :destroy
-  has_many :actions, through: :causes, dependent: :destroy
+  has_many :failure_modes, through: :functions
+  has_many :effects, through: :failure_modes
+  has_many :causes, through: :failure_modes
+  has_many :actions, through: :causes
 
   validates :title, presence: true, uniqueness: { case_sensitive: false }
 end
