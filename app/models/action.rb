a358e888766d5ce1693b61e6847cb099de7aee8c
Thomas Murray <thomas.murray09@gmail.com>
2020-10-06 14:05:05 +0200

    seed all data objects

--- app/models/action.rb
+++ app/models/action.rb
@@ -2,6 +2,6 @@ class Action < ApplicationRecord
   belongs_to :user
   belongs_to :cause
 
-  validates :status, inclusion: { in: ["pending", "in progress", "completed" "cancelled"],
+  validates :status, inclusion: { in: ["pending", "in progress", "completed", "cancelled"],
     message: "%{value} is not a valid status, must be 'pending', 'in progress', 'completed' or 'cancelled'" }
 end
