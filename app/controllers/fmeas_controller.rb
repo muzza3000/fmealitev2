25cc0697fd6a2fb3c4359217b30b8190502541cd
Benjamin Bruckner <benjamin.brueckner@bluewin.ch>
2020-10-06 20:45:01 +0200

    MODAL is not functional, but the view is present

--- app/controllers/fmeas_controller.rb
+++ app/controllers/fmeas_controller.rb
@@ -2,11 +2,20 @@ class FmeasController < ApplicationController
   before_action :set_fmea, only: (:edit)
   def index
     @fmeas = Fmea.all
+    @fmea = Fmea.new
+  end
+
+  def create
+    Fmea.create(fmea_params)
   end
 
   private
 
   def set_fmea
     @fmea = Fmea.find(params[:id])
+  end
+
+  def fmea_params
+    params.require(:fmea).permit(:title, :risk_matrix, :description)
   end
 end
