484eca121ad9ecc5fc27965c0b632ba0a5ba7f79
Benjamin Bruckner <benjamin.brueckner@bluewin.ch>
2020-10-07 14:14:53 +0200

    log in page bug fixed

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,6 +1,8 @@
 module AvatarHelper
   def avatar(user)
-    initials = "#{user.first_name.first.capitalize}#{user.last_name.first.capitalize}"
+    standards = ["BB", "TM"]
+    initials = standards.sample
+    initials = "#{user.first_name.first.capitalize}#{user.last_name.first.capitalize}" unless (user.first_name.nil? || user.last_name.nil?)
     html = "<div class='avatar' style='background-color: #{user.color}'>
       <span class='initials'>#{initials}</span>
       </div>"
