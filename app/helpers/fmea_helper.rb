5bddacb6eb3403f74cfe2488a72baae85524019e
Thomas Murray <thomas.murray09@gmail.com>
2020-10-07 12:58:42 +0200

    top bar details, stats and image placeeholder complete

--- app/helpers/fmea_helper.rb
+++ app/helpers/fmea_helper.rb
@@ -1,7 +1,7 @@
 module FmeaHelper
   def fmea_type_icon(fmea)
     #'Design', 'System' or 'Process'
-    if fmea.fmea_type == "Proces"
+    if fmea.fmea_type == "Process"
       ('<i class="fas fa-tasks"></i>').html_safe
     elsif fmea.fmea_type == "System"
       ('<i class="fas fa-project-diagram"></i>').html_safe
