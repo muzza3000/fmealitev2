0c224f92d8b7a1a1836ab856c4c87906cf81f0c4
Benjamin Bruckner <benjamin.brueckner@bluewin.ch>
2020-10-07 14:38:17 +0200

    fixed initials

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,8 +1,6 @@
 module AvatarHelper
   def avatar(user)
-    standards = ["BB", "TM"]
-    initials = standards.sample
-    initials = "#{user.first_name.first.capitalize}#{user.last_name.first.capitalize}" unless (user.first_name.nil? || user.last_name.nil?)
+    initials = "#{user.first_name.first.capitalize}#{user.last_name.first.capitalize}"
     html = "<div class='avatar' style='background-color: #{user.color}'>
       <span class='initials'>#{initials}</span>
       </div>"
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
 
   has_many :actions
   validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   private
 
