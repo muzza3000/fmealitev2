0e192eaa8399b252e17f7936821c3b648fc94efe
Thomas Murray <thomas.murray09@gmail.com>
2020-10-07 23:00:45 +0200

    added cause#update route; added update to cause controller

--- app/controllers/causes_controller.rb
+++ app/controllers/causes_controller.rb
@@ -1,2 +1,19 @@
 class CausesController < ApplicationController
+  before_action :set_cause, only: [:update]
+
+  def update
+    @cause.update(cause_params)
+    @fmea = @cause.failure_mode.function.fmea
+    redirect_to edit_fmea_path(@fmea)
+  end
+
+  private
+
+  def set_cause
+    @cause = Cause.find(params[:id])
+  end
+
+  def cause_params
+    params.require(:cause).permit(:description, :occurrence)
+  end
 end
