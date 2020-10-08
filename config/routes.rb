35db7885607ff3f5b5937a87c77c90ab68d578c2
Benjamin Bruckner <benjamin.brueckner@bluewin.ch>
2020-10-08 11:32:13 +0200

    login page redirects not to /fmeas dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/user' => "fmeas#index", :as => :user_root
   root to: 'pages#home'
   get '/playground', to: 'pages#playground'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
