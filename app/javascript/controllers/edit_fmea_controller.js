cb3408a5af269a26adfe7cbc8268e610440e321e
Benjamin Bruckner <benjamin.brueckner@bluewin.ch>
2020-10-09 00:08:41 +0200

    the selection of the function and the fm works quite fine already

--- app/javascript/controllers/edit_fmea_controller.js
+++ app/javascript/controllers/edit_fmea_controller.js
@@ -1,29 +1,42 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["function", "one-function", "failure-mode", "cause", "effect"]
+  static targets = ["function", "failuremode"]
 
   connect() {
     // if statement to only display copy button if browser
     // support the action
-    console.log("I work");
-
    }
 
 
   select() {
-    console.log("the funtion is called")
-    let fmeaFunction = event.currentTarget
-    if (!(fmeaFunction.classList.contains("selected-function"))) {
-      fmeaFunction.classList.add("selected-function");
-    } else {
-      fmeaFunction.classList.remove("selected-function");
-    };
-    const FmeaFunctions = this.functionTargets
-    FmeaFunctions.forEach ((f) => {
-      if (f !== fmeaFunction) {
-      f.classList.remove("selected-function");
+    const failureModeButton = document.querySelector("#add-failure-mode");
+    const causeButton = document.querySelector("#add-cause");
+    let element = event.currentTarget;
+
+    if (element.classList.contains("is-function")) {
+      console.log(event.currentTarget);
+      if (!(element.classList.contains("selected-function"))) {
+        element.classList.add("selected-function");
+        failureModeButton.classList.remove("hidden-button")
+      } else {
+        element.classList.remove("selected-function");
+        failureModeButton.classList.add("hidden-button")
       };
-    });
+      // Here, some code should follow which removes the selection of all other function-elements.
+    };
+
+    if (element.classList.contains("is-failuremode")) {
+      console.log(event.currentTarget);
+      if (!(element.classList.contains("selected-failure-mode"))) {
+        element.classList.add("selected-failure-mode");
+        causeButton.classList.remove("hidden-button")
+      } else {
+        element.classList.remove("selected-failure-mode");
+        causeButton.classList.add("hidden-button")
+      };
+      // Here, some code should follow which removes the selection of all other failure-mode-elements.
+    };
+
   };
 };
