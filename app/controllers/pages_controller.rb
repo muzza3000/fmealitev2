696756c95839cd902b2b3cc76323a4ff546f0bc8
Benjamin Bruckner <benjamin.brueckner@bluewin.ch>
2020-10-08 11:26:13 +0200

    bug with sign up form fixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :playground ]
+  skip_before_action :authenticate_user!, only: [ :home, :playground, :new_user_registration  ]
 
   def home
   end
