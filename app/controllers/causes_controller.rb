9715afc176a55169c4b1217e2414eed6fec25f9a
Thomas Murray <thomas.murray09@gmail.com>
2020-10-08 17:15:14 +0200

    added redirect to anchor tag to all controllers and prevent copy icon from moving page

--- app/controllers/causes_controller.rb
+++ app/controllers/causes_controller.rb
@@ -1,10 +1,11 @@
 class CausesController < ApplicationController
+  include CardContentHelper
   before_action :set_cause, only: [:update]
 
   def update
     @cause.update(cause_params)
     @fmea = @cause.failure_mode.function.fmea
-    redirect_to edit_fmea_path(@fmea)
+    redirect_to edit_fmea_path(@fmea, anchor: card_id(@cause))
   end
 
   private
--- app/controllers/effects_controller.rb
+++ app/controllers/effects_controller.rb
@@ -1,10 +1,11 @@
 class EffectsController < ApplicationController
+  include CardContentHelper
   before_action :set_effect, only: [:update]
 
   def update
     @effect.update(effect_params)
     @fmea = @effect.failure_mode.function.fmea
-    redirect_to edit_fmea_path(@fmea)
+    redirect_to edit_fmea_path(@fmea, anchor: card_id(@effect))
   end
 
   private
