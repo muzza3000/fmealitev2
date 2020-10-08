5f46158db41319fb570c008c71ee0ba2e3bbd081
Benjamin Bruckner <benjamin.brueckner@bluewin.ch>
2020-10-08 22:49:31 +0200

    basic selection works

--- app/javascript/controllers/edit_fmea_controller.js
+++ app/javascript/controllers/edit_fmea_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["function", "failure-mode", "cause", "effect"]
+  static targets = ["function", "one-function", "failure-mode", "cause", "effect"]
 
   connect() {
     // if statement to only display copy button if browser
@@ -12,26 +12,11 @@ export default class extends Controller {
 
 
   select() {
-    const fmeaFunction = this.functionTarget
-    console.log("I work too:)")
-    fmeaFunction.classList.add("selected-function")
-  }
-
-  copy() {
-    // In order to copy you must select the text, therefore you must
-    // use and input element. Therefore this is temporarily created
-    // and then removed to perform the copy action.
-    const textToCopy = this.sourceTarget.innerText;
-
-    let myTempInputElement = document.createElement("input");
-    myTempInputElement.type = "text";
-    myTempInputElement.value = textToCopy;
-
-    document.body.appendChild(myTempInputElement);
-
-    myTempInputElement.select();
-    document.execCommand("copy");
-
-    document.body.removeChild(myTempInputElement);
+    const FmeaFunctions = this.functionTargets
+    FmeaFunctions.forEach ((f) => {
+      f.classList.remove("selected-function");
+    });
+    let fmeaFunction = event.currentTarget
+    fmeaFunction.classList.toggle("selected-function")
   }
 }
