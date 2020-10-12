51a73e4931139836ceb9bf28878f7a6665f019ac
Thomas Murray <thomas.murray09@gmail.com>
2020-10-12 11:08:40 +0200

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_191118) do
+ActiveRecord::Schema.define(version: 2020_10_12_090014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_10_08_191118) do
     t.bigint "failure_mode_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "active"
     t.index ["failure_mode_id"], name: "index_causes_on_failure_mode_id"
   end
 
