76d1fa9620de5a3baeac16f35c56c6fee61ca507
Benjamin Bruckner <benjamin.brueckner@bluewin.ch>
2020-10-12 16:22:22 +0200

    update

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -3,7 +3,17 @@ class Cause < ApplicationRecord
   has_many :actions, dependent: :destroy
   validates :description, presence: true
 
+  before_validation :set_confirmed
+
   def parent
     self.failure_mode
+  end
+
+  private
+
+  def set_confirmed
+    if self.confirmed.nil?
+      self.confirmed = true
+    end
   end
 end
