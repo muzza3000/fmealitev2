bf31329db8e05e497cba280305263878c0a65582
Thomas Murray <thomas.murray09@gmail.com>
2020-10-13 13:01:42 +0200

    added redis cloud

--- app/javascript/channels/fmea_channel.js
+++ app/javascript/channels/fmea_channel.js
@@ -9,6 +9,12 @@ export const initFmeaCable = () => {
 
     consumer.subscriptions.create({ channel: "FmeaCollaborationChannel", id: id }, {
       received(data) {
+        // data should be json
+        // type: cause / effect
+        // action: add/remove
+        // payload: (html or ID)
+        // if remove - remove element from page
+        // if add - append the html
         const card = `<div class="cause-card">${data}</div>`
         console.log(card);
         causeContainer.insertAdjacentHTML('beforeend', card)
