4b70e29b8cf9fc1754b2a3f3ab0463c2ee0e8ae6
Thomas Murray <thomas.murray09@gmail.com>
2020-10-15 11:07:15 +0200

    blog post made

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :playground, :new_user_registration  ]
+  skip_before_action :authenticate_user!, only: [ :home, :playground, :new_user_registration, :blog  ]
 
   def home
   end
 
   def playground
+  end
+
+  def blog
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/user' => "fmeas#index", :as => :user_root
   root to: 'pages#home'
   get '/playground', to: 'pages#playground'
+  get '/blog', to: 'pages#blog'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :fmeas, only: %i[index edit create update destroy] do
     member do
